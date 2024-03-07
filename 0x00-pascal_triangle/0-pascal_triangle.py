cbc9b08ace606a2d02284ff325da7cc9d8a81659
Bright Djogatse <brightdjogatse@gmail.com>
2024-03-07 19:48:03 +0000

    fixed code to match pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
 def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to n rows.
+
+    Args:
+        n (int): Number of rows for Pascal's Triangle.
+
+    Returns:
+        list: List of lists representing Pascal's Triangle.
+    """
     if n <= 0:
         return []
 
@@ -7,6 +17,7 @@ def pascal_triangle(n):
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
+            # Calculate current element based on previous row
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
 
