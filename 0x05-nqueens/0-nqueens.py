d05b10c931594c385b8c7ea0a651cfc49004c2d8
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-23 14:40:13 +0000

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,63 +1,54 @@
 #!/usr/bin/python3
 """
-Module
+N queens
 """
+
 import sys
 
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
 
-def is_valid(board, col):
-    """
-    docs
-    """
-    row = board[col]
-    for i in range(col):
-        if board[i] == row or col - i == abs(row - board[i]):
+try:
+    n_q = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
+
+if n_q < 4:
+    print('N must be at least 4')
+    exit(1)
+
+
+def solve_nqueens(n):
+    """docs"""
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
+
+
+def attack_queen(square, queen):
+    """method to use the queen in an attack"""
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
+
+
+def safe_queen(sqr, queens):
+    '''safe queen method to check for queens'''
+    for queen in queens:
+        if attack_queen(sqr, queen):
             return False
     return True
 
 
-def main(n):
-    """
-    docs
-    """
-    board = [-1 for _ in range(n)]
-
-    board[0] = 0
-    i = 0
-    while True:
-        if is_valid(board, i):
-            if i == n - 1:
-                lst = [[i, n] for i, n in enumerate(board)]
-                print(lst)
-                board[i] = -1
-                i -= 1
-            else:
-                i += 1
-        else:
-            if board[i] >= n - 1:
-                board[i] = -1
-                i -= 1
-        while board[i] >= n - 1 and i > 0:
-            board[i] = -1
-            i -= 1
-        if board[i] >= n - 1 and i == 0:
-            break
-        board[i] += 1
-
-
-if __name__ == "__main__":
-    argv = sys.argv
-    min_size = 4
-
-    if (len(argv) != 2):
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(argv[1])
-        if n < min_size:
-            print("N must be at least 4")
-            sys.exit(1)
-        main(n)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
